49294226daeeea9ff84473d4ec9304cc774e6aa9
thaisasnow <contatothaisatas@gmail.com>
2022-07-12 22:03:27 -0300

    Atividade

---
+++ excluirAluno.php
@@ -0,0 +1,18 @@
+<?php
+
+    require_once('repository/AlunoRepository.php');
+    session_start();
+
+    if(fnDeleteAluno($_SESSION['id'])) {
+        $msg = "Sucesso ao apagar";
+    } else {
+        $msg = "Falha ao apagar";
+    }
+
+    
+    unset($_SESSION['id']);
+
+    $page = "listagem-de-alunos.php";
+    setcookie('notify', $msg, time() + 10, "/sga/{$page}", 'localhost');
+    header("location: {$page}");
+    exit;
---
+++ loginSistema.php
@@ -0,0 +1,23 @@
+<?php
+    
+    session_start();
+
+    require_once('repository/LoginRepository.php');
+
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $page = "listagem-de-alunos.php";
+
+    if(!$_SESSION['login'] = fnLogin($email, $senha)) {    
+        $page = "errorPage.php";
+
+     
+        $expire = (time() + 10);
+
+        setcookie('notify', 'Falha ao efetuar o login', 
+            $expire, '/sga/errorPage.php', 'localhost', isset($_SERVER['HTTPS']), true);
+    }
+    
+    header("location: {$page}");
+    exit;
